ce2fa7fec4d6f865a5160f7572c297bb1fb2a319
Rangel Rangelov <rrangelov90@gmail.com>
2024-10-09 11:17:02 +0300

    updated file

--- Code/arraysExercise/1.AddAndSubtract.js
+++ Code/arraysExercise/1.AddAndSubtract.js
@@ -1,18 +1,19 @@
 function addAndSubtract(arr) {
     let originalSum = 0;
     let modifiedSum = 0;
-    let modifiedArr = arr.map((num, index) => {
-        originalSum += num;
-        if (num % 2 === 0) {
-            num += index;
-        } else {
-            num -= index;
-        }
-        modifiedSum += num;
-        return num;
-    });
 
-    console.log(modifiedArr);
+    for (let i = 0; i <arr.length; i++) {
+        originalSum += arr[i];
+
+        if (arr[i] % 2 === 0) {
+            arr[i] += i;
+        } else {
+            arr[i] -= i;
+        }
+        
+        modifiedSum += arr[i];
+    }
+    console.log(arr);
     console.log(originalSum);
     console.log(modifiedSum);
 }
