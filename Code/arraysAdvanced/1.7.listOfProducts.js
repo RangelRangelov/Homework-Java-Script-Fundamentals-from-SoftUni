7cd054d216f5bc0851319506ca05d5b8b087dfb1
Rangel Rangelov <rrangelov90@gmail.com>
2024-10-09 21:40:20 +0300

    updated files

--- Code/arraysAdvanced/1.7.listOfProducts.js
+++ Code/arraysAdvanced/1.7.listOfProducts.js
@@ -2,8 +2,7 @@ function listOfProducts(arr) {
     let sortedArr = arr.sort();
 
     for (let i = 0; i < sortedArr.length; i++) {
-        console.log(`${i + 1}.${sortedArr[i]}`);
-        
+        console.log(`${i + 1}.${sortedArr[i]}`);   
     }    
 }
 
--- Code/arraysAdvanced/1.8.arrayManipulations.js
+++ Code/arraysAdvanced/1.8.arrayManipulations.js
@@ -1,5 +1,39 @@
 function arrayManipulations(arr) {
-    
+    let nums = arr.shift().split(" ").map(x => Number(x));
+    for (const command of arr) {
+        let tokens = command.split(" ");
+        let action = tokens.shift();
+
+        if (action === "Add") {
+            let numToAdd = Number(tokens.shift());
+            nums.push(numToAdd);
+        } else if (action === "Remove") {
+            let numToRemove = Number(tokens.shift());
+            let index = nums.indexOf(numToRemove);
+
+            while (index !== -1) {
+                nums.splice(index, 1);
+                index = nums.indexOf(numToRemove);
+            }
+        } else if (action === "RemoveAt") {
+            let index = Number(tokens.shift());
+            nums.splice(index, 1);
+        } else if (action === "Insert") {
+            let numToAdd = Number(tokens.shift());
+            let index = Number(tokens.shift());
+
+            nums.splice(index, 0, numToAdd);
+        }
+    }
+
+    console.log(nums.join(" "));
 }
 
-arrayManipulations(arr);
+    arrayManipulations
+    ([
+        '4 19 2 53 6 43',
+        'Add 3',
+        'Remove 2',
+        'RemoveAt 1',
+        'Insert 8 3'
+    ]);
