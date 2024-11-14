9184cad9cb5cd067d32df41ce25d31e27ab2b08f
Rangel Rangelov <rrangelov90@gmail.com>
2024-11-14 13:38:53 +0200

    update

---
+++ Code/10.Exr Text Processing/1.revealWords.js
@@ -0,0 +1,7 @@
+function revealWords(wordsStr, text) {
+    let words = wordsStr.split(', ');
+    for (const word of words) {
+        
+    }
+}
+revealWords('great', 'softuni is ***** place for learning new programming languages');
