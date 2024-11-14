ad7c50a725bc932c2a9b89cb22fb335fd92e09d5
Rangel Rangelov <rrangelov90@gmail.com>
2024-11-14 15:39:08 +0200

    update

---
+++ Code/10.Exr Text Processing/2.modernTimesOfHashTag.js
@@ -0,0 +1,5 @@
+function modernTimesOfHashTag(str) {
+    
+}
+
+modernTimesOfHashTag('Nowadays everyone uses # to tag a #special word in #socialMedia');
--- Code/10.Exr Text Processing/5.replaceRepeatingChars.js
+++ Code/10.Exr Text Processing/5.replaceRepeatingChars.js
@@ -1,5 +1,15 @@
 function replaceRepeatingChars(str) {
-    
+    let nonRepeatStr = '';
+    let previousChar = '';
+
+    for (const currChar of str) {
+        if (currChar !== previousChar) {
+            nonRepeatStr += currChar;
+            previousChar = currChar;
+        }
+    }
+
+    console.log(nonRepeatStr);   
 }
 
 replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa');
