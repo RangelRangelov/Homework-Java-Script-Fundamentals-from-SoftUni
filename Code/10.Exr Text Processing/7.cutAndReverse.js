c1e80c938bd07bc47396482a9cb08faca8171007
Rangel Rangelov <rrangelov90@gmail.com>
2024-11-14 12:46:29 +0200

    update

--- Code/10.Exr Text Processing/7.cutAndReverse.js
+++ Code/10.Exr Text Processing/7.cutAndReverse.js
@@ -1,5 +1,13 @@
 function cutAndReverse(arr) {
-    
+   let midIndex = arr.length / 2;
+   let leftHalf = arr.substring(0, midIndex);
+   let rigthhalf = arr.substring(midIndex);
+   
+   let leftHalfReverse = leftHalf.split('').reverse().join('');
+   let rigthHalfReverse = rigthhalf.split('').reverse().join('');
+
+   console.log(leftHalfReverse);
+   console.log(rigthHalfReverse);
 }
 
 cutAndReverse('tluciffiDsIsihTgnizamAoSsIsihT');
