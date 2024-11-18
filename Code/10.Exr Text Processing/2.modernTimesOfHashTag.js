2a9cfe743ce2b29b07a1c860e4a3c207f65d366e
Rangel Rangelov <rrangelov90@gmail.com>
2024-11-18 11:19:17 +0200

    update

--- Code/10.Exr Text Processing/2.modernTimesOfHashTag.js
+++ Code/10.Exr Text Processing/2.modernTimesOfHashTag.js
@@ -1,5 +1,14 @@
-function modernTimesOfHashTag(str) {
-    
+function modernTimesOfHashTag(text) {
+    let hashtags = text.split(' ').filter(word => word.startsWith('#') && word.length > 1);
+
+    for (let hastag of hashtags) {
+        hastag = hastag.substring(1);
+        let pattern = /^[A-Za-z]+$/;
+
+        if (pattern.test(hastag)) {
+            console.log(hastag);
+        }
+    }
 }
 
 modernTimesOfHashTag('Nowadays everyone uses # to tag a #special word in #socialMedia');
